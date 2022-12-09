c76a6367b68b1d40fb8e7f9abb008a9f27402c3c
AleksKutsenko <alonse_97@mail.ru>
2022-12-09 20:04:59 +0300

    bug fix

--- src/validateSchema.js
+++
@@ -1,7 +0,0 @@
-import * as yup from 'yup';
-
-const validateForm = (value, resources) => {
-  const schema = yup.string().url().min(5).notOneOf(resources);
-  return schema.validate(value);
-};
-export default validateForm;
